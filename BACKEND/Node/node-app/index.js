a76b1885a00dd9cb777f83302370764b2fc94855
ChinmayKaitade <chinmaykaitade123@gmail.com>
2025-02-06 16:00:09 +0530

    cli-color code added

--- BACKEND/Node/node-app/index.js
+++ BACKEND/Node/node-app/index.js
@@ -1,1 +1,5 @@
-console.log("Node App");
+const color = require("cli-color");
+
+console.log(color.red("Hello World!"));
+console.log(color.yellow("Hello World!"));
+console.log(color.green("Hello World!"));
