35f555f4410a66b0e903d2cff9409b4f15c849cd
ChinmayKaitade <chinmaykaitade123@gmail.com>
2025-02-13 16:30:27 +0530

    21ES Module Import and Export code added

---
+++ BACKEND/Backend/NodeJS/21ES-Module-Import-Export/index.js
@@ -0,0 +1,15 @@
+// const add = require("./math");
+// const multiply = require("./math");
+// const math = require("./math");
+
+// import multiply from "./math.js";
+import { add, subs, multiply, div, PI } from "./math.js";
+import * as math from "./math.js";
+
+console.log(math.add(5, 10));
+console.log(multiply(70, 10));
+console.log(math.subs(5, 10));
+console.log(div(50, 10));
+console.log(math.PI);
+
+
---
+++ BACKEND/Backend/NodeJS/21ES-Module-Import-Export/math.js
@@ -0,0 +1,21 @@
+const add = (a, b) => {
+  return a + b;
+};
+
+const subs = (a, b) => {
+  return a - b;
+};
+
+const div = (a, b) => {
+  return a / b;
+};
+
+const multiply = (a, b) => {
+  return a * b;
+};
+
+const PI = 3.214;
+
+// export default multiply;
+
+export { add, subs, multiply, div, PI };
