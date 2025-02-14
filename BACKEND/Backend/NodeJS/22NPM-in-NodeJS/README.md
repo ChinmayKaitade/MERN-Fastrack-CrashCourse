92f72f2de568fc84e2f59ef22633445c1a168fb5
ChinmayKaitade <chinmaykaitade123@gmail.com>
2025-02-14 14:28:44 +0530

    NPM in NodeJS code added

---
+++ BACKEND/Backend/NodeJS/22NPM-in-NodeJS/README.md
@@ -0,0 +1,11 @@
+# NPM in Node.js: Install Packages, Commands and Versions! 🚀❤️‍🔥
+
+- npm is a popular package manager which comes bundled with Node.js.
+
+- It is a CLI tool used to install, update, and remove external packages.
+
+- You can also create your own package and publish it on npmjs.com registry.
+
+- Do not confuse npm CLI with npmjs.com, as it’s a registry where most of the packages of Node.js are saved.
+
+- There are alternative Node.js package managers registry like JSR, which we will discuss in future.
---
+++ BACKEND/Backend/NodeJS/22NPM-in-NodeJS/index.js
@@ -0,0 +1,25 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.write("<h1>Homepage Rendered...</h1>");
+    res.end();
+  }
+
+  if (req.url === "/source-code") {
+    res.write("<h1>Source Code Page Rendered...</h1>");
+    res.end();
+  }
+
+  if (req.url === "/contact") {
+    res.setHeader("Content-Type", "text/plain");
+    res.write("<h1Contact Page Rendered...</h1>");
+    res.end();
+  }
+});
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server is listening on PORT:${PORT}🔥`);
+});
