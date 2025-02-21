8acd89bd2d89224f78c31b81f98ac38044aa4367
ChinmayKaitade <chinmaykaitade123@gmail.com>
2025-02-21 16:29:43 +0530

    40env File in ExpressJS code added

---
+++ BACKEND/Backend/ExpressJS/40env-File-in-ExpressJS/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+const app = express();
+const PORT = 4000;
+
+app.get("/", (req, res) => {
+  return res.send("<h1>Homepage Rendered...</h1>");
+});
+
+app.get("/about", (req, res) => {
+  return res.send("<h1>About Page Rendered...</h1>");
+});
+
+app.get("/contact", (req, res) => {
+  return res.send("<h1>Contact Page Rendered...</h1>");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on http://localhost:${PORT}`);
+});
