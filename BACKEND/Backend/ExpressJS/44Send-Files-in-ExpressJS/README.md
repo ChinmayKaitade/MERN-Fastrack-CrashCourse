a0f5c8695acfb2e6f498592c37a8c6aefbace55b
ChinmayKaitade <chinmaykaitade123@gmail.com>
2025-02-22 15:42:30 +0530

    44Send Files in ExpressJS code added

---
+++ BACKEND/Backend/ExpressJS/44Send-Files-in-ExpressJS/README.md
@@ -0,0 +1,6 @@
+# How to send Files in Express JS | Fix `__dirname` Not working in ExpressJS 🚀❤️‍🔥
+
+- You might have many environment variables in your project, and ot's important to validate each one to ensure they are all present and correct.
+- You can either handle this manually or, preferably, use a validation library like Zod.
+- Zod is a TypeScript-first schema declaration and validation library that allows you to define and validate the structure of your data easily.
+- We will be using Zod for various validations in the future, so we'll also use it here to validate environment variables.
---
+++ BACKEND/Backend/ExpressJS/44Send-Files-in-ExpressJS/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+// import { PORT } from "./env.js";
+const app = express();
+import path from "path";
+
+const PORT = process.env.PORT || 4002;
+
+// console.log(__dirname);
+// console.log(__filename);
+
+app.get("/", (req, res) => {
+  // console.log(import.meta.dirname);
+  // console.log(import.meta.url);
+
+  // const __filename = new URL(import.meta.url).pathname;
+  // console.log(__filename);
+
+  // return res.send("<h1>Homepage Rendered...</h1>");
+
+  const homePagePath = path.join(import.meta.dirname, "public", "index.html");
+
+  res.sendFile(homePagePath);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on http://localhost:${PORT}`);
+});
